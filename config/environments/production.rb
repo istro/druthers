cb683511aa52994ee3e3d42cf672698fd674ec07
DBC Apprentice <apprentice@devbootcamp.com>
2012-08-07 15:32:33 -0700

    added devise settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,22 @@ Druthers::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Devise mail settings
+
+   Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+
+   ActionMailer::Base.delivery_method = :smtp
+   ActionMailer::Base.perform_deliveries = true
+   ActionMailer::Base.raise_delivery_errors = true
+
+   ActionMailer::Base.smtp_settings = {
+     :enable_starttls_auto => true,
+     :address        => 'smtp.gmail.com',
+     :port           => 587,
+     :authentication => :plain,
+     :user_name      => 'dbcdruthers@gmail.com',
+     :password       => 'ifihadmydruthers'
+   }
+
 end
