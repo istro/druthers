5a99b445290fc3a25cebb22078e24c974b1c6261
David Lee, + Ivan Stroganov <apprentice+dl+is@devbootcamp.com>
2012-08-13 14:11:00 -0700

    add first level of commenting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+  def create
+    @solution = Solution.find params[:solution_id]
+    @comment = @solution.comments.new (params[:comment])
+    @comment.user = current_user 
+    @comment.save
+
+    redirect_to discussion_solution_path(@solution.discussion, @solution)
+  end
 end
--- spec/integration/solution_pages_spec.rb
+++ spec/integration/solution_pages_spec.rb
@@ -16,7 +16,7 @@ describe 'proposing a solution' do
   context "when a user submits a solution" do
     it "redirects the user to the discussion page with the new solution" do
       visit discussion_path(@discussion)
-      fill_in "Suggest your solution:", :with => "I propose ice cream"
+      fill_in "Suggest your solution:", with: "I propose ice cream"
       click_button "Propose new solution"
       page.should have_content "I propose ice cream"
     end
@@ -39,10 +39,18 @@ describe "commenting on a solution" do
   end
 
   context "when a user views a single solution" do
-    it "provides a comment form" do
+    before :each do
       visit discussion_path(@discussion)
       click_link 'comments'
+    end
+    it "provides a comment form" do
       page.should have_button "Submit"
+    end
+
+    it "her comment submission appears on the page" do
+      fill_in "New Comment", with: "Great solution!"
+      click_button "Submit"
+      page.should have_content "Great solution!"
     end
   end
 end
