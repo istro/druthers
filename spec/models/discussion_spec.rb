5f2c7e84dcac6b3ec5abd3e109ed082e5208709e
David Lee <dlee777@gmail.com>
2012-08-16 09:41:24 -0700

    merge conflicts resolved

--- spec/models/discussion_spec.rb
+++ spec/models/discussion_spec.rb
@@ -3,5 +3,6 @@ require 'spec_helper'
 describe Discussion do
   it {should belong_to :user}
   it {should validate_presence_of :question}
+  it {should ensure_length_of(:question).is_at_most(150) }
   it {should have_many :user_discussions}
 end
