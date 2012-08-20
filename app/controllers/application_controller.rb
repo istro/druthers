33cfdbd2d8bfb12fd652c095ec56e419d8d36e64
GavMo <quavmo@gmail.com>
2012-08-19 18:33:51 -0700

    after login redirects to most recently visited discussion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  after_filter :store_location
 
-  def after_sign_in_path_for(resource)
-  	discussions_path
+  def store_location
+    session[:return_to] = request.fullpath
+  end
+
+
+  def after_sign_in_path_for(resource)                                                                                                                      
+    sign_in_url = url_for(:action => 'new', :controller => 'sessions', :only_path => false, :protocol => 'http')                                            
+    if (request.referer == sign_in_url)                                                                                                                     
+      super                                                                                                                                                 
+    else                                                                                                                                                    
+      request.referer || session[:return_to] || root_path                                                                                         
+    end                                                                                                                                                     
   end
 end
+
+
